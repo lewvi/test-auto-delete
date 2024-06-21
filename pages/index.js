f509fa1f0f687a91a69a5f1d9b4fd34deb2ca597
natthakun.au <natthakun.au@gmail.com>
2024-06-22 02:48:38 +0700

    create : delete list

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,177 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+"use client";
+import React from "react";
+import { Card, Col, Row, Typography } from "antd";
+import { cloneDeep, differenceWith, isEqual} from "lodash";
+import { useMemo, useState } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const data = [
+  {
+    type: "Fruit",
+    name: "Apple",
+  },
+  {
+    type: "Vegetable",
+    name: "Broccoli",
+  },
+  {
+    type: "Vegetable",
+    name: "Mushroom",
+  },
+  {
+    type: "Fruit",
+    name: "Banana",
+  },
+  {
+    type: "Vegetable",
+    name: "Tomato",
+  },
+  {
+    type: "Fruit",
+    name: "Orange",
+  },
+  {
+    type: "Fruit",
+    name: "Mango",
+  },
+  {
+    type: "Fruit",
+    name: "Pineapple",
+  },
+  {
+    type: "Vegetable",
+    name: "Cucumber",
+  },
+  {
+    type: "Fruit",
+    name: "Watermelon",
+  },
+  {
+    type: "Vegetable",
+    name: "Carrot",
+  },
+];
 
 export default function Home() {
+  const [selectItem, setSelectItem] = useState([]);
+
+  const dataList = useMemo(() => {
+    if (data == null || data?.length === 0) return [];
+
+    const checkData = differenceWith(data, selectItem, isEqual);
+
+    return checkData;
+  }, [selectItem]);
+
+  const currentData = useMemo(() => {
+    if (selectItem == null) return [];
+
+    return selectItem;
+  }, [selectItem]);
+
+  const dataFruit = useMemo(() => {
+    if (currentData == null || currentData?.length === 0) return [];
+
+    const findData = currentData?.filter((e) => e?.type === "Fruit");
+
+    return findData;
+  }, [currentData]);
+
+  const dataVegetable = useMemo(() => {
+    if (currentData == null || currentData?.length === 0) return [];
+
+    const findData = currentData?.filter((e) => e?.type === "Vegetable");
+
+    return findData;
+  }, [currentData]);
+
+  const handleSelectItem = (data) => {
+    setSelectItem((pre) => [...pre, data]);
+  };
+
+  const handleRemoveItem = (data) => {
+    let newData = cloneDeep(currentData);
+    const index = newData?.findIndex((e) => e?.name === data?.name);
+
+    newData = [...newData?.slice(0, index), ...newData?.slice(index + 1)];
+
+    setSelectItem([...newData]);
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className="p-5 min-h-[calc(100vh-50px)]">
+      <Card className="bg-neutral-200 h-full">
+        <Row gutter={[8, 8]}>
+          <Col xl={8} md={8} xs={24}>
+            <Card title="All List" className="h-[calc(100vh-80px)]">
+              {dataList?.map((item, index) => {
+                return (
+                  <Card
+                    key={index}
+                    styles={{ body: { padding: 12 } }}
+                    className="mb-2 cursor-pointer"
+                    onClick={() => handleSelectItem(item)}
+                    hoverable
+                  >
+                    <Typography.Text>{item?.name || "N/A"}</Typography.Text>
+                  </Card>
+                );
+              })}
+            </Card>
+          </Col>
+          <Col xl={8} md={8} xs={24}>
+            <Card
+              title="Fruit"
+              className="min-h-[calc(100vh-80px)] cursor-pointer"
+              onClick={() =>
+                handleRemoveItem(
+                  dataFruit?.[
+                    dataFruit?.length - 1 === -1 ? 0 : dataFruit?.length - 1
+                  ]
+                )
+              }
+            >
+              {dataFruit?.map((item, index) => {
+                return (
+                  <Card
+                    key={index}
+                    className="mb-2"
+                    styles={{ body: { padding: 12 } }}
+                    // onClick={() => handleRemoveItem(item)}
+                  >
+                    <Typography.Text>{item?.name || "N/A"}</Typography.Text>
+                  </Card>
+                );
+              })}
+            </Card>
+          </Col>
+          <Col xl={8} md={8} xs={24}>
+            <Card
+              title="Vegetable"
+              className="min-h-[calc(100vh-80px)] cursor-pointer"
+              onClick={() =>
+                handleRemoveItem(
+                  dataVegetable?.[
+                    dataVegetable?.length - 1 === -1 ? 0 : dataVegetable?.length - 1
+                  ]
+                )
+              }
+            >
+              {dataVegetable?.map((item, index) => {
+                return (
+                  <Card
+                    key={index}
+                    className="mb-2"
+                    styles={{ body: { padding: 12 } }}
+                    // onClick={() => handleRemoveItem(item)}
+                  >
+                    <Typography.Text>{item?.name || "N/A"}</Typography.Text>
+                  </Card>
+                );
+              })}
+            </Card>
+          </Col>
+        </Row>
+      </Card>
+    </div>
   );
 }
